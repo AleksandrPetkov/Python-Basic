89a4dabd258d5a33728ca10a068bdff4aad5b3b5
AleksandrPetkov <109108541+AleksandrPetkov@users.noreply.github.com>
2022-09-05 18:30:25 +0300

    Correct the home task for lesson 4

--- lesson_4_hw_1/task1.py
+++ lesson_4_hw_1/task1.py
@@ -1,16 +1,10 @@
 import math
 
 
-def degrees2radians():
+def degrees2radians(a):
     cos_a = a * (math.pi / 180)
-    cos_b = b * (math.pi / 180)
-    cos_c = c * (math.pi / 180)
-    return cos_a, cos_b, cos_c
+    return cos_a
 
 
-a = float(input('Enter the value for "cos_a": '))
-b = float(input('Enter the value for "cos_b": '))
-c = float(input('Enter the value for "cos_c": '))
-res_cos_a, res_cos_b, res_cos_c = degrees2radians()
-print('The answers for task are:\n' '-cos60 is', round(res_cos_a, 3), ';')
-print('-cos45 is', round(res_cos_b, 3), ';\n' '-cos40 is', round(res_cos_c, 3), '.')
+print('The answers for task are:\n' '-cos60 is', round(degrees2radians(60), 3), ';')
+print('-cos45 is', round(degrees2radians(45), 3), ';\n' '-cos40 is', round(degrees2radians(40), 3), '.')
