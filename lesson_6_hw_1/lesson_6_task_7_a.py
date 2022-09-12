9086f4a95e189cc7c22096d5986fbc31568808a3
AleksandrPetkov <109108541+AleksandrPetkov@users.noreply.github.com>
2022-09-12 10:35:43 +0300

    Home task for lesson 6

--- lesson_6_hw_1/lesson_6_task_7_a.py
+++ lesson_6_hw_1/lesson_6_task_7_a.py
@@ -6,48 +6,45 @@ while True:
     if user_number == hidden_number:
         print('Congratulations, you guessed the number!!!')
         break
-    if user_number != hidden_number:
-        choice = input('Do you want to try again, enter yes/no:')
-        if choice == 'no':
+    else:
+        choice = input('Do you want to try again, enter y/n:')
+        if choice == 'n':
             print('Goodbye!')
             break
-        if choice == 'yes':
+        elif choice != 'y' and choice != 'n':
+            continue
+        else:
             choice_2 = input('Do you want continue to guess or computer can try to guess your number,'
                              'enter "i" - you want to continue, "c" - computer try to guess:')
             if choice_2 == 'i':
                 user_number = int(input('Please enter your number from 1 to 10: '))
-
             if choice_2 == 'c':
                 comp_number = random.randint(1, 10)
                 comp_range = [*range(1, 11)]
                 mid = len(comp_range) // 2
-                low = 0
+                low = 1
                 high = len(comp_range)
-                print('Am I guess your number, enter yes/no?', mid)
+                print('Am I guess your number, enter y/n?', mid)
                 user_answer = input()
-                if user_answer == 'yes':
+                if user_answer == 'y':
                     print('I am guess your number!!!')
                     break
-                if user_answer == 'no':
-                    user_answer_2 = input('Is your number less or bigger than mine, enter less/bigger:')
-                    while comp_range[mid] != comp_number and low <= high:
+                if user_answer == 'n':
+                    while mid != comp_number and low <= high:
+                        user_answer_2 = input('Is your number less or bigger than mine, enter less/bigger:')
                         if user_answer_2 == 'bigger':
                             low = mid + 1
-                        else:
+                        if user_answer_2 == 'less':
                             high = mid - 1
                         mid = (low + high) // 2
                         if low > high:
                             print('No value')
                         else:
-                            print('Am I guess your number, enter yes/no?', mid)
-                            user_answer = input()
-                            if user_answer == 'yes':
+                            print('Am I guess your number, enter y/n?', mid)
+                            user_answer2 = input()
+                            if user_answer2 == 'n':
+                                continue
+                            if user_answer2 == 'y':
                                 print('I am guess your number!!!')
-                                print('Goodbye!!!')
                                 break
-                            else:
-                                user_answer_2 = input('My number was less or bigger than your, enter less/bigger:')
-
-
-
-
+        break
