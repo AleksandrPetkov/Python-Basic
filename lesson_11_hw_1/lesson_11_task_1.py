119408daca5052dc0d9f82a1970a08d32d5286aa
AleksandrPetkov <109108541+AleksandrPetkov@users.noreply.github.com>
2022-10-02 17:14:10 +0300

    Home task for lesson 11

--- lesson_11_hw_1/lesson_11_task_1.py
+++ lesson_11_hw_1/lesson_11_task_1.py
@@ -11,7 +11,7 @@ def expected(*expected_types):
         def wrapper(*args, **kwargs):
             func_result = func(*args, **kwargs)
             if not isinstance(func_result, expected_types):
-                raise UnexpectedTypeException('Was expecting types: str, ing')
+                raise UnexpectedTypeException('Was expecting types: str, int')
             else:
                 print(func(*args, **kwargs))
         return wrapper
