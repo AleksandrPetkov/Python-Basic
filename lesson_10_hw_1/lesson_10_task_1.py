674396ced4b630b16d1cfafd2c53a283152d1eb0
AleksandrPetkov <109108541+AleksandrPetkov@users.noreply.github.com>
2022-10-05 10:28:32 +0300

    Corrections for home task for lesson 10

--- lesson_10_hw_1/lesson_10_task_1.py
+++ lesson_10_hw_1/lesson_10_task_1.py
@@ -15,15 +15,20 @@ def copydeep(arg):
         }
     return arg
 
-lst_1 = ['a', 1, (2.0, 0, [75], {'a':2, 'b':1}), ['b'], {'a':2, 'b':1}]
-lst_2 = copydeep(lst_1)
-#вставка во вложенный список
-lst_1[3].append(0)
-#вставка во вложенный кортеж
-lst_1[2][2].append(3)
-lst_1[2][3]['c'] = 5
-#вставка во вложенный словарь
-lst_1[4]['D'] = 5
 
-print(f'Original: {lst_1}\n'
-      f'Function: {lst_2}\n')
+def main():
+    lst_1 = ['a', 1, (2.0, 0, [75], {'a': 2, 'b': 1}), ['b'], {'a': 2, 'b': [1, 3]}]
+    lst_2 = copydeep(lst_1)
+    # вставка во вложенный список
+    lst_1[3].append(0)
+    # вставка во вложенный кортеж
+    lst_1[2][2].append(3)
+    lst_1[2][3]['b'] = 5
+    # вставка во вложенный словарь
+    lst_1[4]['b'] = 5
+    print(f'Original: {lst_1}\n'
+          f'Function: {lst_2}\n')
+
+
+if __name__ == '__main__':
+    main()
