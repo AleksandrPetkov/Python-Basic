53f62a7df6a57a848256952d70740a08dca02cd8
AleksandrPetkov <109108541+AleksandrPetkov@users.noreply.github.com>
2022-10-30 10:28:51 +0200

    Homework 2 for lesson 15

--- lesson_15_hw_2/main.py
+++ lesson_15_hw_2/main.py
@@ -6,19 +6,23 @@ from program.classes.run_program import RunProgram, ProgramError
 
 def main():
     """Run the program."""
-    run_program = RunProgram('товары.csv')
-    run_program.choose_operation()
-    run_program.show_program_result()
     while True:
-        answer = input('Хотите сделать еще что нибудь? (y/n)')
-        if answer == 'y':
-            main()
-        if answer == 'n':
-            print("Завершение работы...")
-            sys.exit()
-        else:
-            ProgramError('Вы ввели неправильный ответ,'
-                         ' попробуйте еще раз.').print_error()
+        run_program = RunProgram('товары.csv')
+        run_program.choose_operation()
+        run_program.show_program_result()
+        continue_run = False
+        while continue_run is False:
+            answer = input('Хотите сделать еще что нибудь? (y/n)')
+            if answer == 'y':
+                continue_run = True
+            elif answer == 'n':
+                print("Завершение работы...")
+                sys.exit()
+            else:
+                ProgramError('Вы ввели неправильный ответ,'
+                             ' попробуйте еще раз.').print_error()
+        if continue_run is True:
+            continue
 
 
 if __name__ == '__main__':
